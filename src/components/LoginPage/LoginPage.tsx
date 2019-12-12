35669faaa517831b3cc68458c7bfc1ee4ca84a66
Tomasz Chybzinski <tomasz.chybzinski@gmail.com>
2019-12-13 00:08:43 +0100

    add LoginPage

--- src/components/LoginPage/LoginPage.tsx
+++ src/components/LoginPage/LoginPage.tsx
@@ -1,9 +1,48 @@
-import React from "react";
+import React, { useState, FormEvent } from "react";
+import { Button, Form, FormGroup, Label, Input } from "reactstrap";
+import styles from "./LoginPage.module.scss";
+import { loginAction } from "./actions";
+import { useDispatch } from "react-redux";
 
-interface IProps {}
+const LoginPage = () => {
+	const [login, setLogin] = useState("");
+	const [password, setPassword] = useState("");
+	const dispatch = useDispatch();
 
-const LoginPage: React.FC<IProps> = (props: IProps): JSX.Element => {
-	return <div>LoginPage</div>;
+	const onSubmit = (e: FormEvent) => {
+		e.preventDefault();
+		dispatch(loginAction(login, password));
+	};
+
+	return (
+		<div className={styles.loginForm}>
+			<Form onSubmit={onSubmit}>
+				<FormGroup className={styles.formGroup}>
+					<Label for="email">Email</Label>
+					<Input
+						type="email"
+						name="email"
+						id="email"
+						placeholder="email.example.com"
+						value={login}
+						onChange={e => setLogin(e.target.value)}
+					/>
+				</FormGroup>
+				<FormGroup>
+					<Label for="password">Password</Label>
+					<Input
+						type="password"
+						name="password"
+						id="password"
+						placeholder="password"
+						value={password}
+						onChange={e => setPassword(e.target.value)}
+					/>
+				</FormGroup>
+				<Button>Login</Button>
+			</Form>
+		</div>
+	);
 };
 
 export default LoginPage;
