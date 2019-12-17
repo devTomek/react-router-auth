2f0dac69157aa4d03891166ad1f80a240a44af0e
Tomasz Chybzinski <tomasz.chybzinski@gmail.com>
2019-12-17 20:53:47 +0100

    remove redux example

--- src/reducers/rootReducer.ts
+++ src/reducers/rootReducer.ts
@@ -1,8 +1,6 @@
 import { combineReducers } from "redux";
-import simpleReducer from "./simpleReducer";
 import apiReducer from "../api/reducer";
 
 export default combineReducers({
-	simpleReducer,
 	apiReducer
 });
