b43b22d3118bc0712aef339036cfb38af6b3b3b2
Tomasz Chybzinski <tomasz.chybzinski@gmail.com>
2019-12-25 19:33:26 +0100

    API tests

--- src/api/API.test.ts
+++ src/api/API.test.ts
@@ -1,9 +1,26 @@
-//todo
-describe.skip("AIP", () => {
+import API, { getJWT } from "./API";
+
+describe("API", () => {
 	describe("getJWT", () => {
-		it("should return JWT", () => {});
-		it("should return undefined", () => {});
+		it("should return JWT", () => {
+			localStorage.setItem("x-auth", "user");
+			expect(getJWT()).toEqual("user");
+			localStorage.removeItem("x-auth");
+		});
+
+		it("should return null", () => {
+			expect(getJWT()).toEqual(null);
+		});
 	});
 
-	describe("login", () => {});
+	describe("login", () => {
+		it("should set user in localStorage", async () => {
+			const login = "login";
+			const password = "password";
+			await API.login(login, password);
+			const user = JSON.stringify({ login, password });
+			expect(localStorage.getItem("x-auth")).toEqual(user);
+			localStorage.removeItem("x-auth");
+		});
+	});
 });
