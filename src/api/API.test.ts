0154d41760f2e6f77938aaba58055c3862ccc2bf
Tomasz Chybzinski <tomasz.chybzinski@gmail.com>
2020-01-12 21:40:11 +0100

    remove login test from API.test

--- src/api/API.test.ts
+++ src/api/API.test.ts
@@ -1,4 +1,4 @@
-import API, { getJWT } from "./API";
+import { getJWT } from "./API";
 
 describe("API", () => {
 	describe("getJWT", () => {
@@ -10,17 +10,6 @@ describe("API", () => {
 
 		it("should return null", () => {
 			expect(getJWT()).toEqual(null);
-		});
-	});
-
-	describe("login", () => {
-		it("should set user in localStorage", async () => {
-			const login = "login";
-			const password = "password";
-			await API.login(login, password);
-			const user = JSON.stringify({ login, password });
-			expect(localStorage.getItem("x-auth")).toEqual(user);
-			localStorage.removeItem("x-auth");
 		});
 	});
 });
